25c0caeb01c337df35d8df28d3c356e788515ac2
Tore Berven <Tore.Berven@student.uib.no>
2019-01-31 11:15:10 +0100

    Made map structure visible

---
+++ src/main/java/inf112.project.RoboRally/Board/pl2.java
@@ -0,0 +1,6 @@
+package inf112.project.RoboRally.Board;
+
+public class pl2 {
+
+    //Placeholder to make map structure visible. To be deleted later.
+}
---
+++ src/main/java/inf112.project.RoboRally/Cards/pl3.java
@@ -0,0 +1,6 @@
+package inf112.project.RoboRally.Cards;
+
+public class pl3 {
+
+    //Placeholder to make map structure visible. To be deleted later.
+}
