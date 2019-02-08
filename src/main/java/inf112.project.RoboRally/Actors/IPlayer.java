4f5e2b8d52bd182260ef80d6cdb8678c21417a02
jdy005 <Jakob.Dyrseth@student.uib.no>
2019-02-08 10:50:50 +0100

    added methods needed for Game to the IPlayer interface

--- src/main/java/inf112.project.RoboRally/Actors/IPlayer.java
+++ src/main/java/inf112.project.RoboRally/Actors/IPlayer.java
@@ -3,6 +3,8 @@ package inf112.project.RoboRally.Actors;
 import inf112.project.RoboRally.Cards.ICard;
 import inf112.project.RoboRally.objects.GridDirection;
 
+import java.util.ArrayList;
+
 public interface IPlayer {
 
     /**
@@ -25,4 +27,20 @@ public interface IPlayer {
      * @param direction of the player
      */
     void movePlayer(ICard card, GridDirection direction);
+
+    int getPlayerDamage();
+
+    void receiveCards(ArrayList<ICard> iCards);
+
+    void addCardsToProgramRegister();
+
+    void removeRemainingCardsInHand();
+
+    ICard revealProgramCardForRegisterNumber(int registerNumber);
+
+    void clearRegister();
+
+    boolean wasDestroyedThisTurn();
+
+    void respawnAtLastArchiveMarker();
 }
