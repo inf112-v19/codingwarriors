75824c1f3def0dca185cd60072956b61c8a0bab1
Tore Berven <tore422@gmail.com>
2019-02-08 16:35:00 +0100

    Made Players able to take damage, and to assess damage taken. Made tests to ensure the correct amount of cards are given based on player damage.

--- src/main/java/inf112.project.RoboRally/actors/Player.java
+++ src/main/java/inf112.project.RoboRally/actors/Player.java
@@ -15,6 +15,7 @@ public class Player implements IPlayer {
     private int x,y;
     private String name;
     private IDeck cardsInHand;
+    private int numberOfDamageTokensRecieved;
 
     public Player(String name, int x, int y) {
         this.playerDirection = GridDirection.NORTH;
@@ -22,6 +23,7 @@ public class Player implements IPlayer {
         this.y = y;
         this.name = name;
         this.cardsInHand = new Deck();
+        this.numberOfDamageTokensRecieved = 0;
     }
 
     public Player(int x, int y) {
@@ -82,7 +84,39 @@ public class Player implements IPlayer {
 
     @Override
     public int getPlayerDamage() {
-        return 0;
+        return this.numberOfDamageTokensRecieved;
+    }
+
+    @Override
+    public void takeOneDamage() {
+        this.numberOfDamageTokensRecieved += 1;
+        this.assessCurrentDamage();
+    }
+
+    @Override
+    public void assessCurrentDamage() {
+        int currentDamageTaken = this.numberOfDamageTokensRecieved;
+        switch (currentDamageTaken) {
+            case 5: this.lockNRegisters(1);
+            case 6: this.lockNRegisters(2);
+            case 7: this.lockNRegisters(3);
+            case 8: this.lockNRegisters(4);
+            case 9: this.lockNRegisters(5);
+            case 10: this.destroyPlayer();
+        }
+    }
+
+    @Override
+    public void destroyPlayer() {
+        this.lives -= 1;
+        if (this.lives <= 0) {
+
+        }
+    }
+
+    @Override
+    public void lockNRegisters(int numberOfRegisters) {
+        // Lock registers from 1 to numberOfRegisters.
     }
 
     @Override
@@ -192,9 +226,7 @@ public class Player implements IPlayer {
         }
     }
 
-
-
-
+    @Override
     public String getName() {
         return this.name;
     }
--- src/test/java/inf112/project/RoboRally/game/GameTest.java
+++ src/test/java/inf112/project/RoboRally/game/GameTest.java
@@ -1,29 +1,84 @@
 package inf112.project.RoboRally.game;
 
+import inf112.project.RoboRally.actors.IPlayer;
+import inf112.project.RoboRally.actors.Player;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
 
     private IGame game;
+    private ArrayList<IPlayer> players;
+    private ArrayList<IPlayer> activePlayers;
 
     @Before
     public void setUp() throws Exception {
         this.game = new Game();
+        this.players = game.getPlayers();
+        this.activePlayers = game.getActivePlayers();
     }
 
     @Test
     public void checkIfActivePlayersIsEqualToPlayersOnStartingANewGame() {
         game.addPlayers();
-        assertEquals(game.getPlayers(), game.getActivePlayers());
+        assertEquals(players, activePlayers);
+    }
+
+    @Test
+    public void aPlayerThatHasTakenZeroDamageDrawsCorrectAmountOfCards() {
+        IPlayer hasTakenZeroDamage = new Player("ZeroDamage", 0, 0);
+        int numberOfCards = game.calculateTheNumberOfCardsThePlayerCanDraw(hasTakenZeroDamage);
+        int maxNumberOfCards = 9;
+        assertEquals(maxNumberOfCards, numberOfCards);
+    }
+
+    @Test
+    public void aPlayerThatHasTakenFiveDamageDrawsCorrectAmountOfCards() {
+        IPlayer hasTakenFiveDamage = new Player("FiveDamage", 0,0);
+        int correctAmountOfCards = 4;
+        for (int i = 0; i < 5; i++) {
+            hasTakenFiveDamage.takeOneDamage();
+        }
+        int numberOfCardsDrawn = game.calculateTheNumberOfCardsThePlayerCanDraw(hasTakenFiveDamage);
+        assertEquals(correctAmountOfCards, numberOfCardsDrawn);
+    }
+
+    @Test
+    public void aPlayerThatHasTakenNineDamageDrawsCorrectAmountOfCards() {
+        IPlayer hasTakenNineDamage = new Player("NineDamage", 0,0);
+        int correctAmountOfCards = 0;
+        for (int i = 0; i < 9; i++) {
+            hasTakenNineDamage.takeOneDamage();
+        }
+        int numberOfCardsDrawn = game.calculateTheNumberOfCardsThePlayerCanDraw(hasTakenNineDamage);
+        assertEquals(correctAmountOfCards, numberOfCardsDrawn);
+    }
+
+    @Test
+    public void aPlayerThatHasTakenTenDamageDrawsCorrectAmountOfCards() {
+        IPlayer hasTakenTenDamage = new Player("TenDamage", 0,0);
+        int correctAmountOfCards = 0;
+        for (int i = 0; i < 10; i++) {
+            hasTakenTenDamage.takeOneDamage();
+        }
+        int numberOfCardsDrawn = game.calculateTheNumberOfCardsThePlayerCanDraw(hasTakenTenDamage);
+        assertEquals(correctAmountOfCards, numberOfCardsDrawn);
     }
 
 
-    @Test
-    public void checkIfPlayerWasDealtCards() {
-        game.startGame();
 
+    @Test
+    public void checkIfEveryPlayerWasDealtCards() {
+        game.startGame();
+        int properStartHandSize = 9;
+        System.out.println("ran");
+        for (IPlayer player: players) {
+            System.out.println("player hand = " + player.getCardsInHand().getSize());
+            assertEquals(properStartHandSize, player.getCardsInHand().getSize());
+        }
     }
 }
